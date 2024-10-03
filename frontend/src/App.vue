bfdc6001d41f9ef5093da05e212d89783d9adb27
aleksandarTcode <aleksandar.trmcic@gmail.com>
2024-10-03 10:26:57 +0200

    added boilerplate table

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,35 +5,51 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
 
-  <button class="bg-blue-500 text-white hover:bg-blue-700 rounded py-2 px-4">Create</button>
+<div class="grid grid-cols-1 gap-2 mt-2 p-4">
+
+  <h2>Posts</h2>
+
+<!--  <button class="bg-blue-500 text-white hover:bg-blue-700 rounded py-2 px-4">Create</button>-->
+
+   <table class="min-w-full divide-y divide-gray-300">
+     <thead>
+     <tr>
+       <th scope="col" class="px-6 py-3 text-start text-sm uppercase">Id</th>
+       <th scope="col" class="px-6 py-3 text-start text-sm uppercase">User</th>
+       <th scope="col" class="px-6 py-3 text-start text-sm uppercase">Title</th>
+       <th scope="col" class="px-6 py-3 text-start text-sm uppercase">Date</th>
+     </tr>
+     </thead>
+
+     <tbody>
+      <tr class="hover-me">
+        <td class="px-6 py-3 text-start text-sm uppercase">4</td>
+        <td class="px-6 py-3 text-start text-sm uppercase">Aleksandar</td>
+        <td class="px-6 py-3 text-start text-sm uppercase">Post1</td>
+        <td class="px-6 py-3 text-start text-sm uppercase">11-3-2024</td>
+      </tr>
+
+      <tr class="hover-me">
+        <td class="px-6 py-3 text-start text-sm uppercase">5</td>
+        <td class="px-6 py-3 text-start text-sm uppercase">Joe</td>
+        <td class="px-6 py-3 text-start text-sm uppercase">Post2</td>
+        <td class="px-6 py-3 text-start text-sm uppercase">12-3-2024</td>
+      </tr>
+
+     </tbody>
+
+     </table>
+
+</div>
+
 
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.hover-me:hover {
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+  background: #8a8a8a;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
